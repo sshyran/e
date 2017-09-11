8e2c54cb32b012f2b3e12bfb04c248148ed1088e
Sergii Shyran <s.shyran@meta.ua>
2017-09-11 08:44:16 -0400

    # curl existence checkup & install from source

--- bash/eba.sh
+++ bash/eba.sh
@@ -5,10 +5,22 @@
 # This script is for Ubuntu Server 16.04 LTS postinstall perks !
 # so... hold on baby !
 
-#  curl -o- https://raw.githubusercontent.com/sshyran/nvm/v0.33.4/install.sh | -e bash ;
+# ehh... do we have curl ? no? grab from source...
+mkdir ~/curl
+cd ~/curl
+vi https://raw.githubusercontent.com//sshyran/e/master/bash/curl.zip
+uncompress curl.zip ~/curl
+# dpkg -i build-essential
+./configure --prefix=~/local
+make install
+curl --version
+;
+vi https://raw.githubusercontent.com//sshyran/nvm/v0.33.4/install.sh | -e bash ;
 #  maybe it's gonna be useful to use   && \\   at the end of each line ?
 
-nvm install node v8.4.0 | -e bash ;
+
+
+nvm install node v8.4.0 ;
 
 # curl -L https://www.npmjs.org/install.sh | -e bash ;
 
