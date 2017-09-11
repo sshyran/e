66ca7c22c61034ddf97d149febc45c673f8e1749
Sergii Shyran <s.shyran@meta.ua>
2017-09-11 09:26:19 -0400

    Update eba.sh

--- bash/eba.sh
+++ bash/eba.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env
 
 { 
 
@@ -8,22 +8,22 @@
 # ehh... do we have curl ? no? grab from source...
 mkdir ~/curl
 cd ~/curl
-vi https://raw.githubusercontent.com//sshyran/e/master/bash/curl.zip
+wget https://raw.githubusercontent.com//sshyran/e/master/bash/curl.zip
 uncompress curl.zip ~/curl
 # dpkg -i build-essential
 ./configure --prefix=~/local
 make install
 curl --version
-;
+#;
 # wanna npm? no?
 #vi https://raw.githubusercontent.com//sshyran/npm/latest/scripts/install.sh | -e bash
-;
+#;
 # nvm?
 #vi https://raw.githubusercontent.com//sshyran/nvm/v0.33.4/install.sh | -e bash 
-;
+#;
 #  maybe it's gonna be useful to use   && \\   at the end of each line ?
 #nvm --version
-;
+#;
 # nvm install node v8.4.0 ;
 
 # curl -L https://www.npmjs.org/install.sh | -e bash ;
